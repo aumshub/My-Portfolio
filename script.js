36abe192d42e4a3dd9348b211cc0e33d9756c4e0
aumshub <aumpatel2930@gmail.com>
2024-12-21 02:35:36 -0500

    major change

--- script.js
+++ script.js
@@ -12,4 +12,69 @@ function toggleMenu() {
     });
 }
 
-scrollAnimation()
+function loaderAnimation(){
+
+  let loader = document.querySelector("#loader");
+
+  setTimeout(() => {
+      loader.style.top = "-100%"
+  }, 3500);
+
+}
+
+
+ // ScrollReveal animations for all section
+
+//  ScrollReveal().reveal('nav', {
+//   duration: 1000,
+//   origin: 'bottom',
+//   distance: '50px',
+//   easing: 'ease-in-out',
+//   reset: true
+// });
+ ScrollReveal().reveal('#profile', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  reset: true,
+});
+
+ScrollReveal().reveal('#about', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  reset: true
+});
+ScrollReveal().reveal('#experience', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  reset: true
+});
+ScrollReveal().reveal('#projects', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  reset: true
+});
+ScrollReveal().reveal('#contact', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  reset: true
+});
+// ScrollReveal().reveal('div', {
+//   duration: 1000,
+//   origin: 'bottom',
+//   distance: '50px',
+//   easing: 'ease-in-out',
+//   reset: true
+// });
+
+loaderAnimation()
+// scrollAnimation()
